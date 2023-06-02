b1305fa82d284db23fdc330f4cfbc39e68f1ac5b
Narcano <zuper1998@gmail.com>
2023-06-02 22:25:54 +0200

    added comments

--- src/interval.rs
+++ src/interval.rs
@@ -19,6 +19,9 @@ impl<TimeType: fmt::Display> fmt::Display for Interval<TimeType> {
 
 
 impl<TimeType: std::cmp::PartialOrd> Interval<TimeType>{
+
+    //Create interval from two TimeTypes, will give a warning if end is before start and will swap
+    // them around
     pub fn new(start: TimeType, end: TimeType) -> Self {
         if start>end{
             warn!("start time is before end time for interval, switching them for now...");
@@ -36,30 +39,38 @@ impl<TimeType: std::cmp::PartialOrd> Interval<TimeType>{
 
     }
 
+
+    // Test if self precedes other
     pub fn precede(&self,other: Interval<TimeType>) -> bool {
         self.end < other.start
     }
 
+    // Test if self meets other
     pub fn meet(&self,other: Interval<TimeType>) -> bool {
         self.end == other.start
     }
 
+    // Test if self overlaps with other
     pub fn  overlaps(&self,other: Interval<TimeType>) -> bool {
         self.end > other.start && self.end < other.end
     }
 
+    // Test if self starts other
     pub fn  starts(&self,other: Interval<TimeType>) -> bool {
         self.start == other.start && self.end < other.end
     }
 
+    // Test if self is during other
     pub fn  during(&self,other: Interval<TimeType>) -> bool {
         self.start > other.start && self.end < other.end
     }
 
+    // Test if self finishes other
     pub fn  finishes(&self,other: Interval<TimeType>) -> bool {
         self.end == other.end && self.start > other.start
     }
 
+    // Test if self is equal with other
     pub fn  equal(&self,other: Interval<TimeType>) -> bool {
         self.start == other.start && self.end == other.end
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
+
+//! Allen's interval algebra is a basic library for intervals and their comparison
+//!
+//! # Example
+//!
+//!
+//! ``` rust
+//!     use allen_interval_algebra::interval::Interval;
+//!
+//!     //Create 3 intervals
+//!     let x = Interval::new(0,10);
+//!     let y = Interval::new(10,14);
+//!     let z = Interval::new(15,16);
+//!
+//!     //Test whether x interval meets z or y
+//!     assert_eq!(x.meet(z),false);
+//!     assert!(x.meet(y))
+//!
+//!
+//! ```
+//!
+
+
 pub mod interval;
+
 
 
 
