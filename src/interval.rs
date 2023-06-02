4d0242adf29b3aa2c57b603ab9d0bb8d1dd3a80e
Narcano <zuper1998@gmail.com>
2023-06-02 23:36:03 +0200

    added comments

--- src/interval.rs
+++ src/interval.rs
@@ -4,7 +4,7 @@ use log::{warn};
 
 
 /// Interval definition with Allen's interval algebra
-/// more info: https://en.wikipedia.org/wiki/Allen%27s_interval_algebra
+/// more info: <https://en.wikipedia.org/wiki/Allen%27s_interval_algebra>
 #[derive(Debug,Clone,Copy)]
 pub struct Interval<TimeType>{
     pub start: TimeType,
@@ -20,8 +20,8 @@ impl<TimeType: fmt::Display> fmt::Display for Interval<TimeType> {
 
 impl<TimeType: std::cmp::PartialOrd> Interval<TimeType>{
 
-    //Create interval from two TimeTypes, will give a warning if end is before start and will swap
-    // them around
+    /// Create interval from two TimeTypes, will give a warning if end is before start and will swap
+    /// them around.
     pub fn new(start: TimeType, end: TimeType) -> Self {
         if start>end{
             warn!("start time is before end time for interval, switching them for now...");
@@ -40,37 +40,37 @@ impl<TimeType: std::cmp::PartialOrd> Interval<TimeType>{
     }
 
 
-    // Test if self precedes other
+    /// Test if self precedes other.
     pub fn precede(&self,other: Interval<TimeType>) -> bool {
         self.end < other.start
     }
 
-    // Test if self meets other
+    /// Test if self meets other.
     pub fn meet(&self,other: Interval<TimeType>) -> bool {
         self.end == other.start
     }
 
-    // Test if self overlaps with other
+    /// Test if self overlaps with other.
     pub fn  overlaps(&self,other: Interval<TimeType>) -> bool {
         self.end > other.start && self.end < other.end
     }
 
-    // Test if self starts other
+    /// Test if self starts other.
     pub fn  starts(&self,other: Interval<TimeType>) -> bool {
         self.start == other.start && self.end < other.end
     }
 
-    // Test if self is during other
+    /// Test if self is during other.
     pub fn  during(&self,other: Interval<TimeType>) -> bool {
         self.start > other.start && self.end < other.end
     }
 
-    // Test if self finishes other
+    /// Test if self finishes other.
     pub fn  finishes(&self,other: Interval<TimeType>) -> bool {
         self.end == other.end && self.start > other.start
     }
 
-    // Test if self is equal with other
+    /// Test if self is equal with other.
     pub fn  equal(&self,other: Interval<TimeType>) -> bool {
         self.start == other.start && self.end == other.end
     }
